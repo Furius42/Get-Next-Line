b631f982d840c60abb60a92f12829ad0fc5e8e03
Vaclav Horacek <vhoracek@c3r4s1.42prague.com>
2025-04-22 20:56:10 +0200

    Campus work upload

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vhoracek <vhoracek@student.42prague.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/14 22:54:27 by vhoracek          #+#    #+#             */
+/*   Updated: 2025/04/14 23:01:41 by vhoracek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char *get_next_line(int fd);
+
+#endif
